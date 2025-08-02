fe0c36a4869b168319683619dabdf4a35c20c0f0
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-08-02 15:18:18 -0700

    Cleaning up sql files so there aren't large gaps in ids and information is more organized

--- fli-guide-backend/seeders/raw_sql/recipe_ranks.sql
+++ fli-guide-backend/seeders/raw_sql/recipe_ranks.sql
@@ -1,6 +1,7 @@
-insert into recipe_ranks (id, name)
-values  (2, 'Fine'),
-        (5, 'Legendary'),
-        (3, 'Notable'),
-        (1, 'Ordinary'),
-        (4, 'Supreme');
+INSERT INTO recipe_ranks (id, name)
+VALUES  
+(2, 'Fine'),
+(5, 'Legendary'),
+(3, 'Notable'),
+(1, 'Ordinary'),
+(4, 'Supreme');
